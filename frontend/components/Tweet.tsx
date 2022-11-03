b746a23a4518c95388d75a132f5ab88a686617fe
Padilha1 <padilha.matheus@hotmail.com>
2022-11-02 21:29:37 -0300

    Setup 1 Class

---
+++ frontend/components/Tweet.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function Tweet () {
+  return (
+    <div>
+        Tweet
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae alias optio perspiciatis quisquam sit exercitationem, esse, tempore impedit culpa temporibus natus tempora suscipit? Corrupti est quaerat recusandae in eos minus?</p>
+    </div>
+  )
+}
+
---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,13 @@
+import { Center, Text } from 'native-base'
+import React from 'react'
+
+function Signin() {
+  return (
+    <Center flex={1} bgColor="gray.900">
+        <Text color="white" fontSize={24}> Sign In</Text>
+        
+    </Center>
+  )
+}
+
+export default Signin
